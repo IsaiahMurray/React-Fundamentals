8eb24c3c4b24eafd9dc5efedc9b3adfc4449b139
Isaiah Murray <murray.isaiah13@gmail.com>
2020-07-30 12:22:45 -0400

    Added timer apps

---
+++ src/components/apps/timer-apps/StopWatchApp.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Stopwatch = () => {
+
+  return (
+    <div>
+       <h1 className="section-title">React Stopwatch!</h1>
+    </div>
+  );
+}
